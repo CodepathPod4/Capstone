dc2976024ecab7cd4bfc62fcec6c471ab7c40c20
juhit101 <juhit101@gmail.com>
2025-08-05 17:27:12 -0400

    fixed album art function, added hidden api keys

--- app/src/main/java/com/example/codepathcapstoneproject/MainActivity.kt
+++ app/src/main/java/com/example/codepathcapstoneproject/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.codepathcapstoneproject
 
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -8,8 +10,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.RequestHeaders
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
+import kotlin.random.Random
 
 private lateinit var trackList: MutableList<String>
 private lateinit var artistList: MutableList<String>
@@ -39,19 +43,33 @@ class MainActivity : AppCompatActivity() {
         setupButton(chill_button, "chill")
         setupButton(study_button, "study")
     }
-    private fun getAlbumArt(track: String, artist: String) {
+
+    private fun getAlbumArt(track: String, artist: String){
         val client = AsyncHttpClient()
-        val url = "https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=1c35e3e663d6cce6a5b0b42b239aa5ac&artist=$artist&track=$track&format=json"
-        client[url, object : JsonHttpResponseHandler() {
-            override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
-                Log.d("APItest", "response successful")
+        val rheaders = RequestHeaders()
+        val ai: ApplicationInfo = application.packageManager
+            .getApplicationInfo(application.packageName, PackageManager.GET_META_DATA)
+        val spotifyKey = ai.metaData.getString("spotifyKey")
+        val url = "https://api.spotify.com/v1/search?q=track:$track+$artist&type=track"
+        rheaders["Authorization"] = spotifyKey
+        client[url, rheaders, null, object : JsonHttpResponseHandler() {
+            override fun onSuccess(statusCode: Int, rheaders: Headers, json: JsonHttpResponseHandler.JSON) {
+                Log.d("APItest", "art response successful")
                 val data = json.jsonObject
-                val art = data.getJSONObject("track").getJSONObject("album").getJSONArray("image").getJSONObject(0).getString("#text")
-                Log.d("APItest", art)
-                trackList.add(track)
-                artistList.add(artist)
-                imageList.add(art)
-                // send to recycler view here
+                Log.d("apidata", data. toString())
+                Log.d("apitest artist", artist)
+                Log.d("apitest track", track)
+                val total = data.getJSONObject("tracks").getInt("total")
+                if (total > 0) {
+                    val art = data.getJSONObject("tracks").getJSONArray("items").getJSONObject(0)
+                        .getJSONObject("album").getJSONArray("images").getJSONObject(0)
+                        .getString("url")
+                    Log.d("Apitest art", art.toString())
+                    trackList.add(track)
+                    artistList.add(artist)
+                    imageList.add(art)
+                    // send to recycler view here
+                }
             }
 
             override fun onFailure(
@@ -60,19 +78,24 @@ class MainActivity : AppCompatActivity() {
                 errorResponse: String,
                 throwable: Throwable?
             ) {
-                Log.d("APIArt Error", errorResponse)
+                Log.d("Apitest album art error", errorResponse)
             }
         }]
-
     }
 
     private fun fetchData(mood: String) {
         val client = AsyncHttpClient()
         val tag = mood
-        val url = "https://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=$tag&api_key=1c35e3e663d6cce6a5b0b42b239aa5ac&format=json"
+        val ai: ApplicationInfo = application.packageManager
+            .getApplicationInfo(application.packageName, PackageManager.GET_META_DATA)
+        val lastfmKey = ai.metaData.getString("lastFMKey")
+        if (lastfmKey != null) {
+            Log.d("apikey", lastfmKey)
+        }
+        val url = "https://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=$tag&api_key=$lastfmKey&format=json"
         client[url, object : JsonHttpResponseHandler() {
             override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
-                Log.d("APItest", "response successful")
+                Log.d("APItest", "mood response successful")
                 for (i in 0 until 20) {
                     val data = json.jsonObject
                     val track = data.getJSONObject("tracks").getJSONArray("track").getJSONObject(i)
@@ -98,7 +121,32 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupButton (button: Button, mood: String) {
         button.setOnClickListener{
-            fetchData(mood)
+            val partyTags = arrayOf("party", "dance", "fun")
+            val sadTags = arrayOf("sad", "melancholy", "acoustic")
+            val workoutTags = arrayOf("workout", "exercise", "upbeat")
+            val chillTags = arrayOf("chillout", "relaxing", "mellow")
+            val studyTags = arrayOf("piano", "lo-fi", "classical")
+            var arr = emptyArray<String>()
+            if (mood.equals("party")){
+                arr = partyTags
+            }
+            if (mood.equals("sad")) {
+                arr = sadTags
+            }
+            if (mood.equals("workout")) {
+                arr = workoutTags
+            }
+            if (mood.equals("chill")) {
+                arr = chillTags
+            }
+            if (mood.equals("study")){
+                arr = studyTags
+            }
+            val num = Random.nextInt(arr.size)
+            var finalMood = arr[num]
+            Log.d("arrayindex", num.toString())
+            Log.d("finalMood", finalMood)
+            fetchData(finalMood)
         }
     }
 }
